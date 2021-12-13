7c25ba1f0a0c82a82fcc810eb9d12c4c8ff2338f
LittleLaGi <yam5421363@gmail.com>
2021-12-13 23:42:37 +0800

    cleanup

--- omg/run.py
+++ omg/run.py
@@ -21,25 +21,14 @@ Tunable Parameters
 6. cross over method (default = single point crossover)
     options:
         single_point
-        two_points
-        uniform
-        scattered
 '''
+
 import sys
 sys.path.append('/home/littlelagi/OhMyGene')
 from printer import Printer
 from wrapper import GAwrapper
 
 def main():
-
-    # args = {
-    #     'gene_count' : 5,
-    #     'gene_bound' : [(0,100), (0,100), (0,100), (0,100), (0,100)],
-    #     'generation_number' : 50,
-    #     'population_size' : 10,
-    #     'mutation_probability' : 0.01,
-    #     'cross_over_method' : 'single_point'
-    # }
 
     args = {
         'gene_count' : 2,
@@ -58,31 +47,6 @@ def main():
     printer = Printer(result)
     printer.printElites()
     printer.plotFitness()
-
-
-
-    """ test Printer """
-    # result = {
-    #     'elites_chromosomes' : [
-    #         [1.1, -1.1], [2.2, -2.2], [3.3, -3.3], [4.4, -4.4]
-    #     ],
-    #     'elites_weights' : [
-    #         [0.1, 0.6, 0.3], [0.2, 0.4, 0.4], [0.1, 0.8, 0.1], [0.4, 0.3, 0.3]
-    #     ],
-    #     'elites_fitness_value' : 0.00123,
-    #     'best_fitness': [
-    #         0.1, 0.09, 0.08, 0.07, 0.06, 0.05, 0.04, 0.03, 0.02 ,0.01
-    #     ],
-    #     'new_solution_rate': [
-    #         10, 8, 8, 5, 3, 4, 4, 4, 2, 2
-    #     ]
-    # }
-    # printer = Printer(result)
-
-    # printer.printElites()
-    # printer.plotFitness()
-    # printer.plotNewSolutionRate()
-
 
 if __name__ == "__main__":
     main()
